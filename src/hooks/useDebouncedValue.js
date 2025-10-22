1ef67088a0ee921f9c9b591e105d23278a204411
Vanessa Gwama <vanessagwama@Vanessas-MacBook-Air.local>
2025-10-22 02:37:16 +0200

    add debounce hook

---
+++ src/hooks/useDebouncedValue.js
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+export default function useDebouncedValue(value, delayMs = 300) {
+  const [debounced, setDebounced] = useState(value);
+  useEffect(() => {
+    const id = setTimeout(() => setDebounced(value), delayMs);
+    return () => clearTimeout(id);
+  }, [value, delayMs]);
+  return debounced;
+}
